a6a773b87ad88403ad5157582940e8823b51de5d
Jerry Cao <2022caojerry@gmail.com>
2025-09-12 14:09:40 -0400

    boomerang added.

--- Assets/Scripts/goriyaSprite.cs
+++ Assets/Scripts/goriyaSprite.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class GoriyaSprite : MonoBehaviour
 {
     public Sprite[] sprites;
+    public Sprite[] boomerangSprites;
 
     void Awake()
     {
@@ -17,6 +18,22 @@ public class GoriyaSprite : MonoBehaviour
         {
             string spriteName = "Zelda/goriya" + (i + 1);
             sprites[i] = Resources.Load<Sprite>(spriteName);
+
+            if (sprites[i] == null)
+            {
+                Debug.LogWarning("Failed to load sprite: " + spriteName);
+            }
+            else
+            {
+                Debug.Log("Loaded sprite: " + sprites[i].name);
+            }
+        }
+        boomerangSprites = new Sprite[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            string spriteName = "Zelda/boomerang" + (i + 1);
+            boomerangSprites[i] = Resources.Load<Sprite>(spriteName);
 
             if (sprites[i] == null)
             {
