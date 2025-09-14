4494ae236de2108f32501b9ae30edf0aeab53be0
Jerry Cao <2022caojerry@gmail.com>
2025-09-14 10:44:41 -0400

    add aquamentus movement

--- Assets/Scripts/AquamentusMovement.cs
+++ Assets/Scripts/AquamentusMovement.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class AquamentusMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 2f;
+    public float moveDistance = 4f;
+
+    private Vector3 startPos;
+    private Vector3 direction = Vector3.left;
+    private float distanceTraveled = 0f;
+
     void Start()
     {
-        
+        startPos = transform.position;
+        direction = Vector3.left;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float moveStep = speed * Time.deltaTime;
+        transform.position += direction * moveStep;
+        distanceTraveled += moveStep;
+
+        if (distanceTraveled >= moveDistance)
+        {
+            direction = -direction;
+            distanceTraveled = 0f;
+        }
     }
 }
